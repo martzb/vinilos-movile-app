05dc7386007eae323e9efaea8014a7cb76efe02d
martzb <224802948+martzb@users.noreply.github.com>
2026-05-01 12:16:25 -0500

    🐛 (fix): comando ADB no encontrado
    Ahora se detecta el comando desde la ruta raiz del proyecto

--- scripts/run_monkey.sh
+++ scripts/run_monkey.sh
@@ -3,6 +3,13 @@
 # Script para ejecutar pruebas de estrés (Monkey Testing) en la app Vinilos.
 # Requisitos: Emulador encendido y aplicación instalada.
 
+# Exportar ADB al PATH si no se encuentra (Solución automática para Mac/Linux)
+if ! command -v adb &> /dev/null
+then
+    echo "adb no encontrado en el PATH. Intentando exportar ruta por defecto de Mac/Linux..."
+    export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
+fi
+
 echo "Iniciando prueba de robustez (Monkey) con 2,000 eventos..."
 adb shell monkey -p com.misw.vinilos --throttle 250 -v 2000 > monkey_report.txt
 echo "Generando Logcat post-prueba..."
